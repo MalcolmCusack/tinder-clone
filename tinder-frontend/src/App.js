74b7183c6365f0542f047458df9d0ec354e4fbbd
malcolmcusack <cusackmalk@gmail.com>
2020-09-27 17:43:44 -0600

    Everything needs to get updated for some reason Added a router and a chat component

--- tinder-frontend/src/App.js
+++ tinder-frontend/src/App.js
@@ -3,13 +3,27 @@ import './CSS/App.css';
 import Header from './Components/Header.js';
 import TinderCards from "./Components/TinderCards.js";
 import SwipeButtons from './Components/SwipeButtons.js';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+ 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <TinderCards />
-      <SwipeButtons />
+      
+      <Router>
+    
+        <Switch>
+          <Route path="/chats">
+            <Header backButton='/'/>
+            <h1>chat page</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <TinderCards />
+            <SwipeButtons />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- tinder-frontend/src/Components/Header.js
+++ tinder-frontend/src/Components/Header.js
@@ -3,21 +3,38 @@ import "../CSS/Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton'
 import ChatIcon from '@material-ui/icons/Chat';
+import { Link, useHistory } from "react-router-dom";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
-function Header() {
+function Header({ backButton }) {
+
+    const history = useHistory();
+
     return (
         <div className='header'>
-            <IconButton>
-                <PersonIcon fontSize="large" className="header_icon"/>
-            </IconButton>
-            <img
-                className='header_logo'
-                src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'
-                alt=''
-            />
-            <IconButton>
-                <ChatIcon fontSize='large' className='header-chat'/>
-            </IconButton>
+            {backButton ? (
+                <IconButton onClick={() => history.replace(backButton)}>
+                    <ArrowBackIosIcon fontSize='large' className='header_icon' />
+                </IconButton>
+            ) : (
+                <IconButton>
+                    <PersonIcon fontSize="large" className="header_icon"/>
+                </IconButton>
+            )}
+
+            <Link to='/'>
+                <img
+                    className='header_logo'
+                    src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'
+                    alt=''
+                />
+            </Link>
+            <Link to='/chats'>
+                <IconButton>
+                    <ChatIcon fontSize='large' className='header-chat'/>
+                </IconButton>
+            </Link>
+            
             
         </div>
     );
